ca2c6ebb14c1418124fc052e6c0176df9c506905
Alqama Bin Sadiq <alqamabinsadiq@gmail.com>
2018-11-13 17:32:55 +0500

    Updates

---
+++ precache-manifest.54f72affd6c197cb39ad3b31b8fc7fa8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "0445beb932bcb5067536",
+    "url": "/static/js/main.0445beb9.chunk.js"
+  },
+  {
+    "revision": "e34b4255ab1c8aeb4063",
+    "url": "/static/js/1.e34b4255.chunk.js"
+  },
+  {
+    "revision": "0445beb932bcb5067536",
+    "url": "/static/css/main.6c226ec9.chunk.css"
+  },
+  {
+    "revision": "efb2590d069c90ca60007350bda298fd",
+    "url": "/index.html"
+  }
+];
